0e9d4f66fc7ab0331383254aeb084b4e56396a29
jayapriyajayapriya205-ship-it <jayapriyajayapriya205@gmail.com>
2026-09-20 15:10:01 +0530

    Add files via upload

---
+++ exp6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+delegate void Notify(string message);
+class Student
+{
+    public string name;
+    public event Notify StudentEvent;
+    public Student(string studentName)
+    {
+        name = studentName;
+    }
+    public void CompleteTask()
+    {
+        Console.WriteLine("\n" + name + " completed the task.");
+        StudentEvent?.Invoke("Task completed successfully.");
+    }
+}
+class Program
+{
+    static void DisplayMessage(string message)
+    {
+        Console.WriteLine("Notification: " + message);
+    }
+    static void Main()
+    {
+        Console.WriteLine("----- Delegates and Events -----");
+        Console.Write("Enter Student Name: ");
+        string name = Console.ReadLine();
+        Student student = new Student(name);
+        student.StudentEvent += DisplayMessage;
+        student.CompleteTask();
+    }
+}
