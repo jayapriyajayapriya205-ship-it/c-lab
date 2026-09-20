94690f868099e2a9ee419f2ab035aca2e791e88c
jayapriyajayapriya205-ship-it <jayapriyajayapriya205@gmail.com>
2026-09-20 15:10:25 +0530

    Add files via upload

---
+++ exp5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Number: " + i);
+            Thread.Sleep(500);
+        }
+    }
+    static void PrintLetters()
+    {
+        for (char c = 'A'; c <= 'E'; c++)
+        {
+            Console.WriteLine("Letter: " + c);
+            Thread.Sleep(500);
+        }
+    }
+static void Main()
+    {
+        Console.WriteLine("----- Threading Demonstration -----");
+        Thread t1 = new Thread(PrintNumbers);
+        Thread t2 = new Thread(PrintLetters);
+        t1.Start();
+        t2.Start();
+        t1.Join();
+        t2.Join();
+        Console.WriteLine("Both threads completed.");
+    }
+}
